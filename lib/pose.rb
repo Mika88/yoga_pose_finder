54a23d6a72a52f591d6d3c0361ec1d564c4edadd
Mika88 <mikaspivak@gmail.com>
2018-09-04 23:46:50 +0000

    Create a Pose class containing attr_accessor and initialize method

--- lib/pose.rb
+++ lib/pose.rb
@@ -1,3 +1,11 @@
 require 'pry'
 
 require_relative '../config/environment'
+
+class Pose
+  attr_accessor :name, :sanskrit_name, :level, :benefits, :description, :instructions
+  @@all = []
+  def initialize
+    @@all << self
+  end
+end
