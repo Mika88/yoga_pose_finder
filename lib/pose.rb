601147841dd7f59c9606906103e48d8783de2380
Mika88 <mikaspivak@gmail.com>
2018-09-05 00:02:32 +0000

    Add an argument of pose_hash to initialize and iterate over hash to assign values to attributes

--- lib/pose.rb
+++ lib/pose.rb
@@ -5,7 +5,15 @@ require_relative '../config/environment'
 class Pose
   attr_accessor :name, :sanskrit_name, :level, :benefits, :description, :instructions
   @@all = []
-  def initialize
+  
+  def initialize(pose_hash)
+     pose_hash.each do |attr, value|
+    self.send "#{attr}=", value
+   end
     @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
