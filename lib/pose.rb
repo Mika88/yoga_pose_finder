d03bf3d25cd9c725b2f61827bd18d0728c46b732
Mika88 <mikaspivak@gmail.com>
2018-09-05 00:34:02 +0000

    Add print_pose method which puts out the profile of the pose

--- lib/pose.rb
+++ lib/pose.rb
@@ -16,4 +16,13 @@ class Pose
   def self.all
     @@all
   end
+  
+  def print_pose
+    puts "------- #{self.name} -------"
+    puts "Sanskrit Name: self.sanskrit_name"
+    puts "Level: self.level "
+    puts "Benefits: self.benefits"
+    puts "Description: self.description"
+    puts "How to do the pose: self.instruction"
+  end
 end
