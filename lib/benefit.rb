98820f6f8eb6509d187eb649484e769a7bbe51ad
Mika88 <mikaspivak@gmail.com>
2018-09-08 19:24:47 +0000

    Create call method in Cli Class

--- lib/benefit.rb
+++ lib/benefit.rb
@@ -1,11 +1,10 @@
-#should be initialized with a name and an empty array of poses
-#should add the poses for each category from scraper
-#should have an array of poses instances
+
 
 require_relative '../config/environment'
 
 class Benefit
   attr_accessor :name, :poses
+  @@all = []
   
   def initialize(name)
     @name = name
@@ -16,6 +15,7 @@ class Benefit
   def self.all
     @@all
   end
+  
   
   def add_pose(pose)
     @poses << pose
--- lib/cli.rb
+++ lib/cli.rb
@@ -8,6 +8,21 @@
 #ask the user if he wants to choose a different pose fron same category, or go back to main list. 
 #create a pose finder with all poses -- alphabetized w.(category)-> allow user to choose from list. 
 
-class Cli
+require_relative '../config/environment'
 
+class Cli
+  def call
+    puts "Hello! welcome to the Yoga-Pose-Finder App."
+    scraper = Scraper.new
+    scraper.benefits_array.each_with_index{|benefint| puts "#{index + 1}. #{benefit}" }
+    
+    puts "Above is a list of different categories of yoga poses," 
+    puts "enter the number of the category you would like to check out." 
+
+    input = gets.strip
+    index = input.to_i - 1 
+    
+     Benefit.all[index]
+   end 
 end
+binding.pry
