3c33da602894d6032a0ead93aeeff7202a396eb3
Mika88 <mikaspivak@gmail.com>
2018-09-16 03:21:48 +0000

    Create find_pose_by_name method

--- lib/pose.rb
+++ lib/pose.rb
@@ -33,6 +33,10 @@ class Pose
     end
   end
   
+  def find_pose_by_name(name)
+    self.all.find{|pose| name == pose.name}
+  end
+  
   def add_attributes(attribute_hash)
       attribute_hash.each do |attr,v|
         self.send "#{attr}=", v
